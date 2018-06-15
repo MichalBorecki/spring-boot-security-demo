ecc002b727a5729afe918bdc94078c35b4e3128b
Michał Borecki <michal.borecki.mb@gmail.com>
2018-06-15 20:11:34 +0200

    "Add SecurityConfirg, change Readme.md"

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # spring-boot-security-demo
-Spring Boot + Spring Security (Demo)
+Project created for learn and test: Spring Boot, Spring Security, Thymeleaf Templates
+
+
+## Tech/framework used 
+
+<b>Built with:<b>
+- [IntelliJ IDEA 2018.1.4](https://www.jetbrains.com/idea/)
+- [phpMyAdmin 4.5.4.1](https://www.phpmyadmin.net/)
+- [Thymeleaf Templates](https://www.thymeleaf.org)
+- [Bootstrap](http://getbootstrap.com)
+
+
+
+
---
+++ src/main/java/pl/kabeonet/springbootsecuritydemo/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package pl.kabeonet.springbootsecuritydemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+* Spring Configuration class to allow configure Spring Security
+*/
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+    }
+}
