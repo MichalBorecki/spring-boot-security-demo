b90c39af142132e36e0cc1e04cfb3bfa10a29b6b
Michał Borecki <michal.borecki.mb@gmail.com>
2018-06-15 19:30:40 +0200

    Add Home page, add HomeController

--- src/test/java/pl/kabeonet/springbootsecuritydemo/SpringBootSecurityDemoApplicationTests.java
+++ src/test/java/pl/kabeonet/springbootsecuritydemo/SpringBootSecurityDemoApplicationTests.java
@@ -1,16 +1,18 @@
-package pl.kabeonet.springbootsecuritydemo;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class SpringBootSecurityDemoApplicationTests {
-
-    @Test
-    public void contextLoads() {
-    }
-
-}
+//package pl.kabeonet.springbootsecuritydemo;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.web.WebAppConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringBootTest(classes = SpringBootSecurityDemoApplication.class)
+//@WebAppConfiguration
+//public class SpringBootSecurityDemoApplicationTests {
+//
+//    @Test
+//    public void contextLoads() {
+//    }
+//
+//}
