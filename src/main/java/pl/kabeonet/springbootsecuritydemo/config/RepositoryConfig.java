943455d730c5b6963c1bed86bcbcf915f1700169
Michał Borecki <michal.borecki.mb@gmail.com>
2018-06-16 03:52:04 +0200

    "Add RepositoryConfig"

---
+++ src/main/java/pl/kabeonet/springbootsecuritydemo/config/RepositoryConfig.java
@@ -0,0 +1,15 @@
+package pl.kabeonet.springbootsecuritydemo.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableAutoConfiguration
+@EntityScan(basePackages = {"pl.kabeonet.springbootsecuritydemo.entity"})
+@EnableJpaRepositories(basePackages = {"pl.kabeonet.springbootsecuritydemo.repository"})
+@EnableTransactionManagement
+public class RepositoryConfig {
+}
