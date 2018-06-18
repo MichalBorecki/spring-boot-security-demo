1485ab370c9dfe61dbc46b9cb15748bf063a07fa
Michał Borecki <michal.borecki.mb@gmail.com>
2018-06-18 22:36:25 +0200

    "Add UserServiceImpl, change UserRepository into interface, add abstract method"

--- src/main/java/pl/kabeonet/springbootsecuritydemo/repository/UserRepository.java
+++ src/main/java/pl/kabeonet/springbootsecuritydemo/repository/UserRepository.java
@@ -3,5 +3,7 @@ package pl.kabeonet.springbootsecuritydemo.repository;
 import org.springframework.data.repository.CrudRepository;
 import pl.kabeonet.springbootsecuritydemo.entity.User;
 
-public class UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    User findByUsername(String username);
 }
---
+++ src/main/java/pl/kabeonet/springbootsecuritydemo/service/UserServiceImpl.java
@@ -0,0 +1,61 @@
+package pl.kabeonet.springbootsecuritydemo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.kabeonet.springbootsecuritydemo.entity.User;
+import pl.kabeonet.springbootsecuritydemo.repository.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Profile("springdatajpa")
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    private EncryptionService encryptionService;
+
+    @Autowired
+    public void setEncryptionService(EncryptionService encryptionService) {
+        this.encryptionService = encryptionService;
+    }
+
+    @Override
+    public List<?> listAll() {
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users::add);
+        return users;
+    }
+
+    @Override
+    public User getById(Long id) {
+        return userRepository.findById(id).get();
+    }
+
+    @Override
+    public User saveOrUpdate(User domainObject) {
+        if (domainObject.getPassword() != null) {
+            domainObject.setEncryptedPassword(encryptionService.encryptString(domainObject.getPassword()));
+        }
+        return userRepository.save(domainObject);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
