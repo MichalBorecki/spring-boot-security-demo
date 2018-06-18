8d8b9989345fca210ada2ab7ece163c1d1b7b016
Michał Borecki <michal.borecki.mb@gmail.com>
2018-06-18 21:34:04 +0200

    "Add user and role entity"

---
+++ src/main/java/pl/kabeonet/springbootsecuritydemo/entity/User.java
@@ -0,0 +1,85 @@
+package pl.kabeonet.springbootsecuritydemo.entity;
+
+import sun.swing.BakedArrayList;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class User {
+    private String userName;
+
+    @Transient
+    private String password;
+
+    private String encryptedPassword;
+    private boolean enabled = true;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable
+    private List<Role> roles = new BakedArrayList<>();
+    private Integer failedLoginAttemps = 0;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEncryptedPassword() {
+        return encryptedPassword;
+    }
+
+    public void setEncryptedPassword(String encryptedPassword) {
+        this.encryptedPassword = encryptedPassword;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+
+    public void addRole(Role role){
+        if(!this.roles.contains(role)){
+            this.roles.add(role);
+        }
+
+        if(!role.getUsers().contains(this)){
+            role.getUsers().add(this);
+        }
+    }
+
+    public void removeRole(Role role){
+        this.roles.remove(role);
+        role.getUsers().remove(this);
+    }
+
+    public Integer getFailedLoginAttemps() {
+        return failedLoginAttemps;
+    }
+
+    public void setFailedLoginAttemps(Integer failedLoginAttemps) {
+        this.failedLoginAttemps = failedLoginAttemps;
+    }
+}
