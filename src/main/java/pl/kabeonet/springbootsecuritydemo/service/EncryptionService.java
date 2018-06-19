ab7082734773b4e00dce6579db499270fc15c1c3
Michał Borecki <michal.borecki.mb@gmail.com>
2018-06-19 23:42:15 +0200

    "Add files for password encryption"

---
+++ src/main/java/pl/kabeonet/springbootsecuritydemo/service/EncryptionService.java
@@ -0,0 +1,6 @@
+package pl.kabeonet.springbootsecuritydemo.service;
+
+public interface EncryptionService {
+    String encryptString(String input);
+    boolean checkPassword(String plainPassword, String encryptedPassword);
+}
