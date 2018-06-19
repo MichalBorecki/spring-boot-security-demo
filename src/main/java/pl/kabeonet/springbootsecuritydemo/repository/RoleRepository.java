75e1bb25c4b83ac39a43d46a336e92c232da0c69
Michał Borecki <michal.borecki.mb@gmail.com>
2018-06-19 23:25:07 +0200

    "Add RoleServiceImpl, change Integer into Long in RoleRepository"

--- src/main/java/pl/kabeonet/springbootsecuritydemo/repository/RoleRepository.java
+++ src/main/java/pl/kabeonet/springbootsecuritydemo/repository/RoleRepository.java
@@ -3,5 +3,5 @@ package pl.kabeonet.springbootsecuritydemo.repository;
 import org.springframework.data.repository.CrudRepository;
 import pl.kabeonet.springbootsecuritydemo.entity.Role;
 
-public interface RoleRepository extends CrudRepository<Role, Integer> {
+public interface RoleRepository extends CrudRepository<Role, Long> {
 }
---
+++ src/main/java/pl/kabeonet/springbootsecuritydemo/service/RoleServiceImpl.java
@@ -0,0 +1,45 @@
+package pl.kabeonet.springbootsecuritydemo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import pl.kabeonet.springbootsecuritydemo.entity.Role;
+import pl.kabeonet.springbootsecuritydemo.repository.RoleRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Profile("springdatajpa")
+public class RoleServiceImpl implements RoleService{
+
+    private RoleRepository roleRepository;
+
+    @Autowired
+    public void setRoleRepository(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public List<?> listAll() {
+        List<Role> roles = new ArrayList<>();
+        roleRepository.findAll().forEach(roles::add);
+        return roles;
+    }
+
+
+    @Override
+    public Role getById(Long id) {
+        return roleRepository.findById(id).get();
+    }
+
+    @Override
+    public Role saveOrUpdate(Role domainObject) {
+        return roleRepository.save(domainObject);
+    }
+
+    @Override
+    public void delete(Long id) {
+        roleRepository.deleteById(id);
+    }
+}
